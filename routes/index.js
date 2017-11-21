1c086c38885872c3ba1a505a4ea2aaa89b951445
lchayoun <liel.chayoun@intel.com>
2017-11-21 19:31:15 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'consul_telemetry_exporter' });
+});
+
+module.exports = router;
---
+++ routes/metrics.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const client = require('prom-client');
+const rp = require('request-promise');
+const _ = require('underscore');
+
+const router = express.Router();
+
+const hostname = process.env.HOSTNAME;
+
+/* GET metrics listing. */
+router.get('/', function(req, res, next) {
+  collect().then(function (metrics) {
+    res.render('metrics', { metrics: metrics });
+  });
+});
+
+module.exports = router;
+
+function collect() {
+  const options = {
+    uri: 'http://' + hostname + ':8500/v1/agent/metrics',
+    json: true
+  };
+
+  return rp(options).then(createPrometheusMetrics);
+}
+
+function createPrometheusMetrics(result) {
+  const registry = new client.Registry();
+  const metrics = {};
+  result.Gauges.forEach(_handleGauge);
+  result.Counters.forEach(_handleCounter);
+  result.Samples.forEach(_handleCounter);
+  return registry.metrics();
+
+  function _handleCounter(counter) {
+    _setGauge(counter.Name + '_count', _.extend({'statistic': 'count'}, counter.Labels), counter.Count);
+    _setGauge(counter.Name + '_sum', _.extend({'statistic': 'sum'}, counter.Labels), counter.Sum);
+    _setGauge(counter.Name + '_min', _.extend({'statistic': 'min'}, counter.Labels), counter.Min);
+    _setGauge(counter.Name + '_max', _.extend({'statistic': 'max'}, counter.Labels), counter.Max);
+    _setGauge(counter.Name + '_mean', _.extend({'statistic': 'mean'}, counter.Labels), counter.Mean);
+    _setGauge(counter.Name + '_stddev', _.extend({'statistic': 'stddev'}, counter.Labels), counter.Stddev);
+  }
+
+  function _handleGauge(gauge) {
+    _setGauge(gauge.Name, gauge.Labels, gauge.Value);
+  }
+
+  function _setGauge(name, labels, value) {
+    labels.host = hostname;
+    let metricName = name.split('.').join('_');
+    metricName = metricName.split('-').join('_');
+    const gaugeMetric = metrics[metricName] || new client.Gauge({
+      name: metricName.toLowerCase(),
+      help: metricName.toLowerCase() + '_help',
+      labelNames: Object.keys(labels),
+      registers: [registry]
+    });
+    gaugeMetric.set(labels, value);
+    metrics[metricName] = gaugeMetric;
+  }
+}
