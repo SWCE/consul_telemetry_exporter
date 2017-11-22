d2c8159e9e96bc509cdbe798f7e13a36929a55f3
lchayoun <liel.chayoun@intel.com>
2017-11-22 12:43:52 +0200

    added test module

---
+++ test/test.spec.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const expect = require('expect.js');
+
+describe('busywait.js', function () {
+
+  it('Should test nothing', function() {
+    expect(true).to.be(true);
+  });
+
+});
