8488a523356895074c34fc4385868d5b828ee407
Roi Ezra <ezraroi@gmail.com>
2017-11-22 08:32:42 +0200

    consul_telemetry_exporter-1 expose api for retrieving the version (#2)

---
+++ routes/version.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const package = require('./../package.json');
+const router = express.Router();
+
+/* GET metrics listing. */
+router.get('/', function (req, res) {
+    collect().then(function (metrics) {
+        res.send(package.version);
+    }).catch(function (err) {
+        res.status(500).send(err);
+    });
+});
+
+module.exports = router;
